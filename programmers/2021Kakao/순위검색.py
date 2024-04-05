76db1d9dfeb15cfaf80d8db9da25a1c5a890251b
seunguri <safg0421@gmail.com>
2024-04-05 23:06:05 +0900

    prog: bisect

--- programmers/2021Kakao/순위검색.py
+++ programmers/2021Kakao/순위검색.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from itertools import combinations
 
-def solution(info, query):
+def solution_0(info, query):
     answer = []
 
     info_hash = defaultdict(list)
@@ -55,3 +55,80 @@ def solution(info, query):
         answer.append(score_len - start)
 
     return answer
+
+'''
+1. Clarify the problem
+    - info 개수: 1~50,000
+    - query 개수: 1~100,000
+2. Define your approach
+    - 조건마다 info index -> query 
+    - query 교집합
+3. Propose a solution
+    - 조건 key, info value
+4. Propose a alternative solution
+5. Implement the solution
+'''
+
+def solution_0(info, query):
+    answer = []
+    score = []
+    condition = {}
+    score = {}
+
+    for i in range(len(info)):
+        con = info[i].split(" ")
+        for j in range(3):
+            condition[con[j]].append(i)
+        ss = score.get(con[3], [])
+        ss.append(i)
+
+    return answer
+
+from bisect import bisect_left
+from collections import defaultdict
+from itertools import combinations
+
+def solution(info, query):
+    answer = []
+    dic = {}
+    comb = [0, 1, 2, 3]
+    for i in info:
+        person = i.split()
+        conditions = person[:-1]
+        score = int(person[-1])
+        for j in range(5):
+            for k in list(combinations(comb, j)):
+                temp = conditions.copy()
+                for idx in k:
+                    temp[idx] = '-'
+                key = ''.join(temp)
+                if key in dic:
+                    dic[key].append(score)
+                else:
+                    dic[key] = [score]
+    
+    for value in dic.values():  
+        value.sort()
+
+    for i in query:
+        q_list = []
+        for j in i.split():
+            if j == 'and':
+                continue
+            q_list.append(j)
+
+        target = int(q_list[-1])
+        key = ''.join(q_list[:-1])
+
+        if key in dic:
+            hubo_list = dic[key]
+
+            index = bisect_left(hubo_list, target)
+            answer.append(len(hubo_list) - index)
+        else:
+            answer.append(0)
+            continue
+
+    return answer
+
+print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
